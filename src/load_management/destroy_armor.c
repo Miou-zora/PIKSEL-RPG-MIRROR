db7d28443bd98f705a631c61598b3ae324c8c7b4
miou-zora <alexandre.franquet@epitech.eu>
2022-04-24 02:28:54 +0200

    FIX : get all the file with get_file and patch free error in destroy_armor

--- src/load_management/destroy_armor.c
+++ src/load_management/destroy_armor.c
@@ -6,12 +6,13 @@
 */
 
 #include "struct_var.h"
+#include "my.h"
 
 void destroy_armor(armor_t **armor)
 {
     if (*armor) {
         if ((*armor)->name) {
-            freen((void **)(*armor)->name);
+            freen((void **)&((*armor)->name));
         }
         if ((*armor)->sprite_data) {
             destroy_sprite_data(&((*armor)->sprite_data));
