176bd3dc06a54ff9b892bd13ad8be701d6cb2268
Kentin Paille <kentin.paille@epitech.eu>
2022-04-26 16:55:45 +0200

    FIX struct ennemy

--- tests/ennemy/test_ennemy_main.c
+++ tests/ennemy/test_ennemy_main.c
@@ -12,16 +12,15 @@
 void init_sprite(game_t *game)
 {
     game->ennemy = malloc(sizeof(ennemy_t));
-    game->ennemy->sprite = malloc(sizeof(ennemy_sprite_t));
-    game->ennemy->sprite->max_left = 580;
-    game->ennemy->sprite->clock = sfClock_create();
-    game->ennemy->sprite->sprite_data = malloc(sizeof(sprite_data_t));
-    game->ennemy->sprite->sprite_data->texture =
+    game->ennemy->max_left = 580;
+    game->ennemy->clock = sfClock_create();
+    game->ennemy->sprite_data = malloc(sizeof(sprite_data_t));
+    game->ennemy->sprite_data->texture =
     sfTexture_createFromFile("assets/ennemy/wolf.png", NULL);
-    game->ennemy->sprite->sprite_data->sprite = sfSprite_create();
-    game->ennemy->sprite->sprite_data->pos = (sfVector2f){250, 250};
-    game->ennemy->sprite->sprite_data->scale = (sfVector2f){1.5, 1.5};
-    game->ennemy->sprite->sprite_data->rect = (sfIntRect){0, 0, 145, 115};
+    game->ennemy->sprite_data->sprite = sfSprite_create();
+    game->ennemy->sprite_data->pos = (sfVector2f){250, 250};
+    game->ennemy->sprite_data->scale = (sfVector2f){1.5, 1.5};
+    game->ennemy->sprite_data->rect = (sfIntRect){0, 0, 145, 115};
 }
 
 int initialize_var(game_t *game)
@@ -40,7 +39,7 @@ int initialize_var(game_t *game)
 void display(game_t *game)
 {
     
-    sfRenderWindow_drawSprite(game->window, game->ennemy->sprite->sprite_data->sprite, NULL);
+    sfRenderWindow_drawSprite(game->window, game->ennemy->sprite_data->sprite, NULL);
     return;
 }
 
@@ -48,8 +47,8 @@ void manage_mouse_click(sfMouseButtonEvent mouseButton, game_t *game)
 {
     game->click[0] = mouseButton.x;
     game->click[1] = mouseButton.y;
-    game->distance[0] = (int)(game->ennemy->sprite->sprite_data->pos.x - game->click[0]);
-    game->distance[1] = (int)(game->ennemy->sprite->sprite_data->pos.y - game->click[1]);
+    game->distance[0] = (int)(game->ennemy->sprite_data->pos.x - game->click[0]);
+    game->distance[1] = (int)(game->ennemy->sprite_data->pos.y - game->click[1]);
 }
 
 
@@ -69,14 +68,14 @@ ennemy_t *move_texture(ennemy_t *ennemy, int left, int height, sfClock *clock)
 
     
     if (time > 100000) {
-        ennemy->sprite->sprite_data->rect.top = height;
-        if (ennemy->sprite->max_left >= ennemy->sprite->sprite_data->rect.left + left) {
-            ennemy->sprite->sprite_data->rect.left += left;
+        ennemy->sprite_data->rect.top = height;
+        if (ennemy->max_left >= ennemy->sprite_data->rect.left + left) {
+            ennemy->sprite_data->rect.left += left;
         } else {
-            ennemy->sprite->sprite_data->rect.left = 0;
+            ennemy->sprite_data->rect.left = 0;
         }
-        sfSprite_setTextureRect(ennemy->sprite->sprite_data->sprite,
-        ennemy->sprite->sprite_data->rect);
+        sfSprite_setTextureRect(ennemy->sprite_data->sprite,
+        ennemy->sprite_data->rect);
         sfClock_restart(clock);
     }
     return (ennemy);
@@ -87,23 +86,20 @@ ennemy_t *move_ennemy(sfClock *clock, ennemy_t *ennemy, int click[2], int distan
     int time = sfClock_getElapsedTime(clock).microseconds;
     
     if ((float)((float)time / (float)100 >= 1)) {
-        if (ennemy->sprite->sprite_data->pos.x < click[0]
-        || ennemy->sprite->sprite_data->pos.x > click[0]) {
-            printf("%f\n", ((float)distance[0] / (float)(abs(distance[1]) + (float)abs(distance[0]))));
-            ennemy->sprite->sprite_data->pos.x -= (float)distance[0] / (float)(abs(distance[1]) + (float)abs(distance[0]));
+        if (ennemy->sprite_data->pos.x < click[0]
+        || ennemy->sprite_data->pos.x > click[0]) {
+            ennemy->sprite_data->pos.x -= (float)distance[0] / (float)(abs(distance[1]) + (float)abs(distance[0]));
             if (distance[0] > 0)
-                ennemy = move_texture(ennemy, 148, 0, ennemy->sprite->clock);
+                ennemy = move_texture(ennemy, 148, 0, ennemy->clock);
             else
-                ennemy = move_texture(ennemy, 148, 115, ennemy->sprite->clock);
+                ennemy = move_texture(ennemy, 148, 115, ennemy->clock);
         }
-        if (ennemy->sprite->sprite_data->pos.y < click[1]
-        || ennemy->sprite->sprite_data->pos.y > click[1]) {
-            ennemy->sprite->sprite_data->pos.y -= (float)distance[1] / (float)(abs(distance[1]) + (float)abs(distance[0]));
-            printf("%f\n", ((float)distance[0] / (float)(abs(distance[1]) + (float)abs(distance[0]))));
+        if (ennemy->sprite_data->pos.y < click[1]
+        || ennemy->sprite_data->pos.y > click[1]) {
+            ennemy->sprite_data->pos.y -= (float)distance[1] / (float)(abs(distance[1]) + (float)abs(distance[0]));
         }
-        sfSprite_setPosition(ennemy->sprite->sprite_data->sprite, ennemy->sprite->sprite_data->pos);
+        sfSprite_setPosition(ennemy->sprite_data->sprite, ennemy->sprite_data->pos);
         sfClock_restart(clock);
-        my_printf("test");
     }
     return (ennemy);
 }
@@ -115,14 +111,14 @@ int main(void)
 
     if (initialize_var(game) == 84)
         return (84);
-    sfSprite_setPosition(game->ennemy->sprite->sprite_data->sprite,
-    game->ennemy->sprite->sprite_data->pos);
-    sfSprite_setTexture(game->ennemy->sprite->sprite_data->sprite,
-    game->ennemy->sprite->sprite_data->texture, sfTrue);
-    sfSprite_setTextureRect(game->ennemy->sprite->sprite_data->sprite,
-    game->ennemy->sprite->sprite_data->rect);
-    sfSprite_scale(game->ennemy->sprite->sprite_data->sprite,
-    game->ennemy->sprite->sprite_data->scale);
+    sfSprite_setPosition(game->ennemy->sprite_data->sprite,
+    game->ennemy->sprite_data->pos);
+    sfSprite_setTexture(game->ennemy->sprite_data->sprite,
+    game->ennemy->sprite_data->texture, sfTrue);
+    sfSprite_setTextureRect(game->ennemy->sprite_data->sprite,
+    game->ennemy->sprite_data->rect);
+    sfSprite_scale(game->ennemy->sprite_data->sprite,
+    game->ennemy->sprite_data->scale);
     sfRenderWindow_setFramerateLimit(game->window, 60);
     while (sfRenderWindow_isOpen(game->window)) {
         display(game);
