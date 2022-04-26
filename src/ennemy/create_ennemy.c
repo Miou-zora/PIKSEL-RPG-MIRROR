720933e62fb6014d6c63d2c7a15899ffb2450913
Kentin Paille <kentin.paille@epitech.eu>
2022-04-26 16:09:55 +0200

    ADD move wolf without collision

--- src/ennemy/create_ennemy.c
+++ src/ennemy/create_ennemy.c
@@ -16,10 +16,6 @@ ennemy_t *create_ennemy(void)
         ennemy->type_ennemy = NOT_SET;
         ennemy->name = NULL;
         ennemy->id = 0;
-        ennemy->health = 0;
-        ennemy->armor = 0;
-        ennemy->power = 0;
-        ennemy->speed = 0;
         ennemy->sprite_data = NULL;
     }
     return (ennemy);
