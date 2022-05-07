a86d14220d37c7ce06863277e68c6d2ec847f7d1
Kentin Paille <kentin.paille@epitech.eu>
2022-05-08 01:34:22 +0200

    ADD pos on lab

--- src/entities/enemy/update_enemy.c
+++ src/entities/enemy/update_enemy.c
@@ -9,15 +9,15 @@
 #include "math.h"
 #include "my.h"
 
-void change_enemy_direction(sfVector2f *enemy_core, sfVector2f *pos,
+/*void change_enemy_direction(sfVector2f *enemy_core, sfVector2f *pos,
 enemy_t *enemy)
 {
     if (enemy_core->x - pos->x > 0) {
-        // sfSprite_setTextureRect(enemy->actual_animator->sprite_data->sprite, (sfIntRect){0, 0, -enemy->actual_animator->size_image.x, enemy->actual_animator->size_image.y});
+        //sfSprite_setTextureRect(enemy->actual_animator->sprite_data->sprite, (sfIntRect){0, 0, -enemy->actual_animator->size_image.x, enemy->actual_animator->size_image.y});
     } else {
-        // sfSprite_setTextureRect(enemy->actual_animator->sprite_data->sprite, (sfIntRect){0, 0, enemy->actual_animator->size_image.x, enemy->actual_animator->size_image.y});
+        //sfSprite_setTextureRect(enemy->actual_animator->sprite_data->sprite, (sfIntRect){0, 0, enemy->actual_animator->size_image.x, enemy->actual_animator->size_image.y});
     }
-}
+}*/
 
 static void move_enemy_to_pos_animator(sfVector2f *enemy_core, sfVector2f *pos,
 float distance, enemy_t *enemy)
@@ -30,7 +30,7 @@ float distance, enemy_t *enemy)
         return;
     } else
         re_set_animator(&(enemy->actual_animator), &(enemy->animator_moving));
-    change_enemy_direction(enemy_core, pos, enemy);
+    //change_enemy_direction(enemy_core, pos, enemy);
     scaling = distance / (enemy->base_speed * enemy->stat.speed);
     if (scaling <= 1) {
         enemy->pos.x = pos->x - (enemy->actual_animator->size_image.x *
@@ -45,7 +45,6 @@ float distance, enemy_t *enemy)
 
 static void move_enemy_to_pos(enemy_t *enemy, sfVector2f pos)
 {
-
     float distance = 0;
     sfVector2f enemy_core;
 
