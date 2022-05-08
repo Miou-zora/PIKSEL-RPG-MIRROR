5737e1fd064057e67f7cd4735e1f6b8a32127ab2
miou-zora <alexandre.franquet@epitech.eu>
2022-05-08 15:36:48 +0200

    FIX : display healthbar for scale

--- src/entities/enemy/spawn_enemy.c
+++ src/entities/enemy/spawn_enemy.c
@@ -30,8 +30,8 @@ enemy_t *spawn_mob_enemy(sfVector2f pos, sfVector2i spawn_info)
     enemy->animator_moving->clock_data->framerate_seconds = 0.1;
     enemy->agro_distance = 1000.f;
     enemy->nest_particle = NULL;
-    enemy->healthbar = create_framebuffer(enemy->actual_animator->size_image.x,
-    20);
+    enemy->healthbar = create_framebuffer(enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x, 20);
     return (enemy);
 }
 
@@ -44,8 +44,8 @@ enemy_t *spawn_mini_boss_animator(enemy_t *enemy)
     enemy->agro_distance = 1000.f;
     enemy->nest_particle = create_nest_particle(500, (sfVector2i){200, 200});
     enemy->nest_particle->color = (sfColor){255, 255, 255, 255};
-    enemy->healthbar = create_framebuffer(enemy->actual_animator->size_image.x,
-    20);
+    enemy->healthbar = create_framebuffer(enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x, 20);
     return (enemy);
 }
 
@@ -86,8 +86,8 @@ enemy_t *spawn_boss_enemy_animator(enemy_t *enemy)
     enemy->animator_moving->clock_data->framerate_seconds = 0.05;
     enemy->actual_animator = enemy->animator_standing;
     enemy->nest_particle->color = (sfColor){255, 0, 255, 255};
-    enemy->healthbar = create_framebuffer(enemy->actual_animator->size_image.x,
-    20);
+    enemy->healthbar = create_framebuffer(enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x, 20);
     return (enemy);
 }
 
