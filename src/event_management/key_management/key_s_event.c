0c1253517a95fb58b5be153f0b284422cb155a64
Kentin Paille <kentin.paille@epitech.eu>
2022-05-08 14:56:56 +0200

    FIX move player on menu

--- src/event_management/key_management/key_s_event.c
+++ src/event_management/key_management/key_s_event.c
@@ -11,7 +11,10 @@
 int manage_key_s(game_t *game)
 {
     if (game->event.key.code == sfKeyS
-    && game->background->menu->pause->display_pause == false) {
+    && game->background->menu->pause->display_pause == false
+    && game->background->scene_background != MENU
+    && game->background->scene_background != SETTINGS
+    && game->background->scene_background != CINEMATIC) {
         game->player->move_down = true;
     }
     return (0);
--- src/event_management/key_management/key_z_event.c
+++ src/event_management/key_management/key_z_event.c
@@ -11,7 +11,10 @@
 int manage_key_z(game_t *game)
 {
     if (game->event.key.code == sfKeyZ
-    && game->background->menu->pause->display_pause == false) {
+    && game->background->menu->pause->display_pause == false
+    && game->background->scene_background != MENU
+    && game->background->scene_background != SETTINGS
+    && game->background->scene_background != CINEMATIC) {
         game->player->move_up = true;
     }
     return (0);
