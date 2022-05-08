3c20f28c980c37457d1c6840c6af71dd7f62486b
miou-zora <alexandre.franquet@epitech.eu>
2022-05-08 21:46:23 +0200

    FIX : coding style

--- src/entities/enemy/player_attack_enemy.c
+++ src/entities/enemy/player_attack_enemy.c
@@ -13,20 +13,20 @@ void sword_attack(sfVector2f enemy_pos,
 sfVector2f player_pos, enemy_t *enemy, bool direction)
 {
     sfIntRect rect_ene = {enemy_pos.x, enemy_pos.y,
-    enemy->actual_animator->size_image.x * enemy->actual_animator->sprite_data->scale.x,
-    enemy->actual_animator->size_image.y * enemy->actual_animator->sprite_data->scale.y};
+    enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x,
+    enemy->actual_animator->size_image.y *
+    enemy->actual_animator->sprite_data->scale.y};
     sfIntRect rect_player = {player_pos.x + 30, player_pos.y, 125, 200};
 
     if (direction == true) {
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 10;
-        }
     } else {
         rect_player.left -= 30;
         rect_player.left -= 200;
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 15;
-        }
     }
 }
 
@@ -34,20 +34,20 @@ void spear_attack(sfVector2f enemy_pos,
 sfVector2f player_pos, enemy_t *enemy, bool direction)
 {
     sfIntRect rect_ene = {enemy_pos.x, enemy_pos.y,
-    enemy->actual_animator->size_image.x * enemy->actual_animator->sprite_data->scale.x,
-    enemy->actual_animator->size_image.y * enemy->actual_animator->sprite_data->scale.y};
+    enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x,
+    enemy->actual_animator->size_image.y *
+    enemy->actual_animator->sprite_data->scale.y};
     sfIntRect rect_player = {player_pos.x + 30, player_pos.y + 30, 300, 100};
 
     if (direction == true) {
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 25;
-        }
     } else {
         rect_player.left -= 30;
         rect_player.left -= 300;
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 100;
-        }
     }
 }
 
@@ -55,20 +55,20 @@ void gloves_attack(sfVector2f enemy_pos,
 sfVector2f player_pos, enemy_t *enemy, bool direction)
 {
     sfIntRect rect_ene = {enemy_pos.x, enemy_pos.y,
-    enemy->actual_animator->size_image.x * enemy->actual_animator->sprite_data->scale.x,
-    enemy->actual_animator->size_image.y * enemy->actual_animator->sprite_data->scale.y};
+    enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x,
+    enemy->actual_animator->size_image.y *
+    enemy->actual_animator->sprite_data->scale.y};
     sfIntRect rect_player = {player_pos.x + 30, player_pos.y - 30, 100, 300};
 
     if (direction == true) {
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 25;
-        }
     } else {
         rect_player.left -= 30;
         rect_player.left -= 200;
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 100;
-        }
     }
 }
 
@@ -76,20 +76,20 @@ void gun_attack(sfVector2f enemy_pos,
 sfVector2f player_pos, enemy_t *enemy, bool direction)
 {
     sfIntRect rect_ene = {enemy_pos.x, enemy_pos.y,
-    enemy->actual_animator->size_image.x * enemy->actual_animator->sprite_data->scale.x,
-    enemy->actual_animator->size_image.y * enemy->actual_animator->sprite_data->scale.y};
+    enemy->actual_animator->size_image.x *
+    enemy->actual_animator->sprite_data->scale.x,
+    enemy->actual_animator->size_image.y *
+    enemy->actual_animator->sprite_data->scale.y};
     sfIntRect rect_player = {player_pos.x + 10, player_pos.y + 30, 500, 20};
 
     if (direction == true) {
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 20;
-        }
     } else {
         rect_player.left -= 20;
         rect_player.left -= 500;
-        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL)) {
+        if (sfIntRect_intersects(&rect_ene, &rect_player, NULL))
             enemy->stat.actual_life -= 60;
-        }
     }
 }
 
