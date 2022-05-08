2ec24e071f301e390db20dddaf211bf55e157667
miou-zora <alexandre.franquet@epitech.eu>
2022-05-08 22:08:10 +0200

    FIX : coding style

--- src/npc/create_npc_text_zone.c
+++ src/npc/create_npc_text_zone.c
@@ -10,12 +10,12 @@
 bool create_npc_zero_text(npc_t *npc)
 {
     if (create_text_zone(&npc->dialogue,
-    "                                                                        \
-                                        \
-                           Find the thief and take revenge !!        \
-                                                \n\n\n                     \
-                                                                        \
-                    press f to continue") == 84)
+    "                                                                        "
+    "                                        "
+    "                           Find the thief and take revenge !!        "
+    "                                                \n\n\n                   "
+    "                                                                        "
+    "                      press f to continue") == 84)
         return (true);
     npc->dialogue->text_clock = create_clock_data();
     set_framerate_clock_data(npc->dialogue->text_clock, 0.03);
@@ -25,12 +25,12 @@ bool create_npc_zero_text(npc_t *npc)
 bool create_npc_one_text(npc_t *npc)
 {
     if (create_text_zone(&npc->dialogue,
-    "                                                                        \
-                                        \
-                           You will be teleported in the forest        \
-                                                \n\n\n                     \
-                                                                        \
-                    press f to continue") == 84)
+    "                                                                        "
+    "                                        "
+    "                           You will be teleported in the forest        "
+    "                                                \n\n\n                   "
+    "                                                                         "
+    "                     press f to continue") == 84)
         return (true);
     npc->dialogue->text_clock = create_clock_data();
     set_framerate_clock_data(npc->dialogue->text_clock, 0.03);
@@ -40,12 +40,12 @@ bool create_npc_one_text(npc_t *npc)
 bool create_npc_two_text(npc_t *npc)
 {
     if (create_text_zone(&npc->dialogue,
-    "                                                                        \
-                                        \
-                           You will be teleported in the city        \
-                                                \n\n\n                     \
-                                                                        \
-                    press f to continue") == 84)
+    "                                                                        "
+    "                                        "
+    "                           You will be teleported in the city        "
+    "                                                \n\n\n                   "
+    "                                                                         "
+    "                     press f to continue") == 84)
         return (true);
     npc->dialogue->text_clock = create_clock_data();
     set_framerate_clock_data(npc->dialogue->text_clock, 0.03);
@@ -55,12 +55,12 @@ bool create_npc_two_text(npc_t *npc)
 bool create_npc_three_text(npc_t *npc)
 {
     if (create_text_zone(&npc->dialogue,
-    "                                                                        \
-                                        \
-                           You will be teleported in the lab        \
-                                                \n\n\n                     \
-                                                                        \
-                    press f to continue") == 84)
+    "                                                                        "
+    "                                        "
+    "                           You will be teleported in the lab        "
+    "                                                \n\n\n                   "
+    "                                                                        "
+    "                      press f to continue") == 84)
         return (true);
     npc->dialogue->text_clock = create_clock_data();
     set_framerate_clock_data(npc->dialogue->text_clock, 0.03);
@@ -70,12 +70,12 @@ bool create_npc_three_text(npc_t *npc)
 bool create_npc_four_text(npc_t *npc)
 {
     if (create_text_zone(&npc->dialogue,
-    "                                                                        \
-                                        \
-                           Good job you have make your revenge :)        \
-                                                \n\n\n                     \
-                                                                        \
-                    press f to continue") == 84)
+    "                                                                        "
+"                                        "
+"                           Good job you have make your revenge :)        "
+"                                                \n\n\n                     "
+"                                                                        "
+"                    press f to continue") == 84)
         return (true);
     npc->dialogue->text_clock = create_clock_data();
     set_framerate_clock_data(npc->dialogue->text_clock, 0.03);
